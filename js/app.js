c55e805844dabf3830785a447166ae76481d9215
rakibul58 <rhrahi14@gmail.com>
2022-09-09 23:13:46 +0600

    responsive slide and card

--- js/app.js
+++ js/app.js
@@ -6,23 +6,29 @@
 };
 
 
-const showProducts = (products) =>
+const showProducts = products =>
 {
     const productContainer = document.getElementById('product-container');
     products.forEach(product => {
 
         const newDiv = document.createElement('div');
         newDiv.classList.add('col');
+        const stars = getstars(product.rating.rate);
+        // console.log(stars.innerHTML)
         newDiv.innerHTML = `
         
-        <div class="h-100 card bg-opacity-50">
+        <div class="h-100 card">
             <img height="250px" src="${product.image}" class="p-4 card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${product.title}</h5>
-                <p class="card-text">This is a longer card with supporting text below as...</p>
+                <p class="card-text">${product.description.length<50 ? product.description : product.description.slice(0,50)+'...'}</p>
             </div>
-            <div class="card-footer border-0 bg-white pb-4">
-                <button class="my-btn rounded text-white fw-semibold">Buy Now</button>
+            <div class="card-footer border-0 bg-white pb-2 g-0">
+                <div class="d-flex justify-content-between">
+                    <h5 class="text-warning">${product.price}$<h5>
+                    <p id="stars">${stars.innerHTML}</p>
+                </div>
+                <button class="my-btn rounded text-white fw-semibold">Buy</button>
                 <button class="my-btn rounded text-white fw-semibold">Details</button>
             </div>
         </div>
@@ -31,6 +37,32 @@ const showProducts = (products) =>
         productContainer.appendChild(newDiv);
         
     });
+}
+
+
+const getstars = numOfStars =>
+{
+    const intNumOfStars = parseInt(numOfStars);
+    const newSpan = document.createElement('span');
+    
+    for(let i=0 ; i<intNumOfStars ; ++i){
+        const newI = document.createElement('i');
+        newI.classList.add('fa-solid' , 'fa-star' , 'text-warning');
+        newSpan.appendChild(newI);
+    }
+    if(intNumOfStars<5)
+    {
+        const newI = document.createElement('i');
+        newI.classList.add('fa-solid' , 'fa-star-half-stroke' , 'text-warning');
+        newSpan.appendChild(newI);
+    }
+    for(let i=0 ; i<5-intNumOfStars-1 ; ++i){
+        const newI = document.createElement('i');
+        newI.classList.add('fa-regular' , 'fa-star' , 'text-warning');
+        newSpan.appendChild(newI);
+    }
+
+   return newSpan;
 }
 
 
